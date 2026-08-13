10648c4893fec71d6d3a680a8a7ab61bb3e1c119
ramya58-hub <ramyakavili5@gmail.com>
2026-08-13 09:52:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,29 @@
+function greet(name) {
+
+    return "Hello " + name;
+
+}
+
+const add = function(a, b) {
+
+    return a + b;
+
+};
+
+const multiply = (a, b) => {
+
+    return a * b;
+
+};
+
+document.getElementById("declaration").textContent =
+
+    greet("Akhila");
+
+document.getElementById("expression").textContent =
+
+    "Addition = " + add(10, 20);
+
+document.getElementById("arrow").textContent =
+
+    "Multiplication = " + multiply(5, 4);
